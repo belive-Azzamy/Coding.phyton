ea8b2c530cf930be8ad0299732cacca9b8a3ad4b
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-03-27 18:07:36 +0800

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,47 @@
+a, b, c, = 10, 20, 30
+
+print(a)
+print(b)
+print(c)
+
+student_name = "ali"
+print(student_name)
+
+first_name = "Aisyah"
+last_name  = "qonita"
+
+print(first_name + " " + last_name)
+print(f"Hello, {first_name} {last_name}")
+
+print(f"{a} + {b} = {a+b}")
+
+print(f"{10 * '#'}")
+print(10 * "=")
+print("I" + " " + 5 * "Love" + " " + "Studyyyyyyyyyyyyyyyyyyy" + " " + "HAHAHAAA")
+
+print('"python" is a programing language')
+print("maybe 'python' is fun.Wait, i mean i mean the 'python' is realy fun")
+
+
+print("\"Hello World!\"")
+print("\\Hello\\ World!")
+print("Hello \n World!")
+print("Hello\bWorld!")
+print("Hello\tWorld")
+
+full_name = "Azzamy Muttaqin"
+
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.capitalize())
+print(full_name.split())
+print(full_name.swapcase())
+
+
+
+
+
+
+
+
