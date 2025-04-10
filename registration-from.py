1a0ea3cb88a3f46bef07a0bc13ad2afa90219012
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-04-10 18:20:48 +0800

    Add files via upload

---
+++ registration-from.py
@@ -0,0 +1,22 @@
+print("Hello, walcome user")
+name    = input("Enter your name: ")
+address = input("Enter your address: ")
+email   = input("ENter your email: ")
+
+print(f"Congratulation {name} from {address} with {email}, your registering is succsesfully")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
