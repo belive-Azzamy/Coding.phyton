3509f4e12bb1021518e16a6cb683ab29bd1e43fe
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-04-10 18:21:16 +0800

    Add files via upload

---
+++ data-type.py
@@ -0,0 +1,21 @@
+name = "Azzam"
+print(name)
+print(type(name))
+
+age = 15
+print(age)
+print(type(age))
+
+height = 1.67
+print(height)
+print(type(height))
+
+
+fruits = ["apple", "banana", "lemon"]
+print(fruits)
+print(type(fruits))
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print(type(numbers))
+
