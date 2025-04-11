72feabf340ba4f877c9a3a7d66f0b1e6d944925d
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-04-11 17:57:15 +0800

    Add files via upload

---
+++ data-type-list.py
@@ -0,0 +1,22 @@
+the_prophet = "Muhammad"
+print(the_prophet)
+print(type(the_prophet))
+
+class_number = 7
+print(class_number)
+print(type(class_number))
+
+weight = 172.5
+print(weight)
+print(type(weight))
+
+shy_person = True
+print(shy_person)
+print(type(shy_person))
+
+motorbike_brand = None
+print(motorbike_brand)
+print(type(motorbike_brand))
+
+
+
