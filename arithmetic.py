36dbed4a7e4134a038ab7b74183db63758f6dac8
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-04-17 18:09:45 +0800

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,68 @@
+"""
+    operator           Name
+    ========          ============
+       +               addition
+       -               subtraction
+       *               mulltiple
+       /               
+       %               
+       **              
+       //              
+"""
+
+
+angka1 = 20
+angka2 = 12
+
+add = angka1 + angka2
+subs = angka1 - angka2
+multi = angka1 * 5
+division = angka1 / 2
+floor_division = angka1 // 5
+mod = angka1 % 5
+expo = angka1 ** 3
+
+# Plus
+print(f"{angka1} + {angka2} = {add}")
+
+# Minus
+print(f"{angka1} - {angka2} = {subs}")
+
+# Mulltiple
+print(f"{angka1} * 5 = {multi}")
+
+# Division
+print(f"{angka1} / 2 = {division}")
+
+# Floor division
+print(f"{angka1} // 5 = {floor_division}")
+
+# Mod
+print(f"{angka1} % 5 = {mod}")
+
+# Expo
+print(f"{angka1} ** 3 = {expo}")
+
+a = 10
+print(a)
+a += 2 # a = a + 2
+print(a)
+a -= 5
+print(a)
+a *= 3
+print(a)
+a /= 2
+print(a)
+a //= 2
+print(a)
+a %= 2
+print(a)
+a **= 3
+print(a)
+
+
+
+
+
+
+
