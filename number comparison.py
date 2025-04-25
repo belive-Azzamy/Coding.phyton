b5f65f4c6e00e8bf6e857f119fa47048f4e28546
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-04-25 17:46:29 +0800

    Add files via upload

---
+++ number comparison.py
@@ -0,0 +1,26 @@
+number1 = 18
+number2 = 20
+
+print(number1 == number2)
+print(number1 != number2)
+print(number1 > number2)
+print(number1 < number2)
+print(number1 >= number2)
+print(number1 <= number2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
