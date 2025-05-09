e73d6865686a7f91153b1735b4b76954df736a8e
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-05-09 18:06:14 +0800

    Add files via upload

---
+++ project-dict.py
@@ -0,0 +1,14 @@
+Hobby = input("Enter your hobby: ")
+Function_hobby = input("Enter your function hobby: ")
+
+hobby_dict = dict(name=Hobby, function=Function_hobby)
+
+
+hobby_dict["Year"] = "2021"
+hobby_dict["Place"] = "Jakarta"
+print(hobby_dict)
+print(type(hobby_dict))
+
+list.add = {"year" : 2021, "Place" : "Jakarta"}
+hobby_dict.update(list.add)
+print(hobby_dict)
