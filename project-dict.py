a4d6e986bc18142524a22a3cf554e1e88f70fa48
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-05-09 18:05:28 +0800

    Delete project-dict.py

--- project-dict.py
+++
@@ -1,8 +0,0 @@
-Hobbyname = dict(Name="Razy", Hobby="Video Editor")
-print(Hobbyname)
-print(type(Hobbyname))
-
-Hobbyname["Year"] = "2021"
-Hobbyname["Place"] = "Home"
-print(Hobbyname)
-print(type(Hobbyname))
