b1a37e4396cde40717d674fde82ce97282df2b54
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-05-16 16:55:44 +0800

    Add files via upload

---
+++ number-slicer.py
@@ -0,0 +1,21 @@
+# create a variable called 'numbers'
+# creat a list of numbers 1 - 250 using list(range())
+numbers = range(1,251)
+print(list(numbers))
+
+# print a number from index 15
+numbers = list(numbers)
+print(numbers)
+print(numbers[15])
+
+# print an eight number from the back
+# clue : negative indexing
+print(numbers[-8])
+
+# print a group of numbers, start from 55(number) until 67
+print(numbers[54:66])
+
+
+# print a group of even numbers, starts from 70(number) until 240
+# clue : use step parameter
+print(numbers[69:240:2])
