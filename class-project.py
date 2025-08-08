0a215b0f8548da3251dd6d29426b391d31be14d3
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-08-08 18:06:59 +0800

    Add files via upload

---
+++ class-project.py
@@ -0,0 +1,12 @@
+class Motor:
+
+    def __init__(self, merk_motor, warna_motor, kecepatan_motor):
+        self.merk = merk_motor
+        self.warna = warna_motor
+        self.kecepatan = kecepatan_motor
+    
+    def laporan(self):
+        print(f'Motor dengan merk {self.merk} berwarna {self.warna} sedang melaju dengan kecepatan {self.kecepatan} km/jam')
+
+honda = Motor('Vario 150 gen 2', 'GlossyBlack', '80')
+honda.laporan()
